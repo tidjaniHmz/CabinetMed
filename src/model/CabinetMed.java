6549be4abf74852d21521dd8810a904518f4b51b
Abdelrahim Chernai <abdelrahim.chernai@gmail.com>
2021-08-17 00:07:31 +0100

    initial commit

---
+++ src/model/CabinetMed.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package model;
+
+/**
+ * @author Abdelrahim
+ *
+ */
+public class CabinetMed {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
